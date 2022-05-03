1467ede02f350fc3081eff47071c3016942246e5
roo <roo116@nc.rr.com>
2022-05-03 01:25:04 -0400

    created arrays for alpha, numeric and special chars

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+// Assignment code here
+
+//arrays
+var strAlphaCap = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'X','Z'];
+var strAlphaSmall = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var spcChar = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '|', '}', '{', ']', '[', ';', ':', '?', '/', '>', '<', '.', ',']
+
+
+//prompt for the number of characters
+
+
+//prompt for numbers
+
+//prompt for special characters
+
+
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
