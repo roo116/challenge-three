a5ecb76137d3ae8c6325b42159d9e33246caea55
roo <roo116@nc.rr.com>
2022-05-03 03:56:12 -0400

    added logic to test if an entry is a number and an integer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,35 @@
 // Assignment code here
 
 //arrays
-var strAlphaCap = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'X','Z'];
+var strAlphaCap = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'X', 'Z'];
 var strAlphaSmall = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var spcChar = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '|', '}', '{', ']', '[', ';', ':', '?', '/', '>', '<', '.', ',']
 
 
 //prompt for the number of characters
+
+
+var numCheck = function () {
+  val = Number(numChar) ;
+  
+  if (val === NaN || val === "") {
+    alert("You need to enter a whole number.  Please try again")  
+  }
+
+  else if(Number.isInteger(val) !== true) {
+    alert("You need to enter a whole number.  Please try again");
+  }
+  else{
+    window.alert("Your password will be " + val + " characters long.");
+    }
+  };
+
+  var numChar = window.prompt("How many characters do you want your password to be?");
+numCheck();
+
+
+
 
 
 //prompt for numbers
@@ -19,16 +41,16 @@ var spcChar = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', 
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
