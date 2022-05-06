caabda94260e298f1902ba01e3f00f029cf94d9f
roo <roo116@nc.rr.com>
2022-05-06 00:43:05 -0400

    created code to push capital letters into the target array for the new password, if user wants caps

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,13 @@ var strAlphaCap = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', '
 var strAlphaSmall = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var spcChar = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '|', '}', '{', ']', '[', ';', ':', '?', '/', '>', '<', '.', ',']
+var newArray = [];
 // var questions ["How many characters do you want your password to be?", ""]
 //global variables
 var btn = document.querySelector("button");
 var pwdLength = 0;
-var ucase = 0;
-var lcase = 0;
+// var ucase = true;
+// var lcase = true;
 
 
 //prompt for the number of characters
@@ -28,40 +29,58 @@ var lcase = 0;
 
 
 function numCheck() {
-  pwdLength = prompt("How many characters do you want your password to be?  Please enter a number between 8 and 128");
-    if( pwdLength !== null ){
-      pwdLength = Number(pwdLength);
-      if (pwdLength < 8 || pwdLength > 128 || Number.isInteger(pwdLength) !== true) {
+  pwdLength = prompt("How many characters do you want your password to be?  Please enter a number between 8 and 128"); 
+  if( pwdLength === null ){
+    alert("You hit cancel.  So we are going to just go quiet.  Click the 'Generate Password' button to restart.  Have a good day!!!");
+      return;
+  }
+
+  pwdLength = Number(pwdLength)
+
+  if (pwdLength < 8 || pwdLength > 128 || Number.isInteger(pwdLength) !== true) {
         alert("Ok.  So you need to enter a number between 8 and 128.  You know?  Like the sign said?")
             return numCheck() 
       }else {
         alert("Cool.  Your password is going to be " + pwdLength + " characters long.");
-      }
+
+    }
+
+    var ucase = confirm("do you want capital letters in the password?")
+    if (!ucase) {
+      confirm("you will have no caps in the password")
     } else {
-      alert("You hit cancel.  So we are going to just go quiet.  Click the 'Generate Password' button to restart.  Have a good day!!!");
-      return;
+      for(i=0; i < strAlphaCap.length; i++)
+      newArray.push(strAlphaCap[i]);
+      console.log(newArray);
+    }
+   
+
     };
-      
-            
-}
+           
+   
+// function createPasswordArray() {
+//   var ucase = confirm("do you want uppercase characters in your password? Click OK for 'yes' or Cancel for 'no'");
+//   console.log(">> Inside the function ucase is " + ucase);
+//   if (!ucase) {
+//     alert("No caps for you!!!");
+//     return;
+//   } else {
+//     alert("You will have capital letters in your password");
+//     var newArray = []
 
 
-//   pwdLength = Number(pwdLength);
-
-//   
 //   }
-//   else {
-//     alert("Your password will be " + pwdLength + " characters long.");
-//   }
+// }
+ 
 
-//   pwdLength = pwdLength
-// };
 
-function ucaseCheck() {
-  ucase = prompt("how many uppercase characters would you like in your password?");
-}
+//   ucase = Number(ucase)
+//   if (ucase !== null) 
+//     ucase = Number(ucase)
+// }
 
 btn.addEventListener("click", numCheck)
+
 
 // var numChar = window.prompt("How many characters do you want your password to be?");
 
